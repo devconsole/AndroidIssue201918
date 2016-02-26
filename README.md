8fede6f5036214783b496061c1f00341885dc404
Markus Wiederkehr <markus.wiederkehr@gmail.com>
2016-02-26 13:21:30 +0100

    Initial version.

---
+++ README.md
@@ -0,0 +1,3 @@
+Sample app to illustrate Android issue 201918.
+
+See https://code.google.com/p/android/issues/detail?id=201918
---
+++ app/src/main/java/com/test/androidissue201918/MainActivity.java
@@ -0,0 +1,24 @@
+package com.test.androidissue201918;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+}
